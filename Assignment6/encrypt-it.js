77645507b0a9d928a897239c62f023d89f735652
JohnSwa <98369610+JohnSwa@users.noreply.github.com>
2023-05-18 21:51:41 -0700

    assignment 6 finish

--- Assignment6/encrypt-it.js
+++ Assignment6/encrypt-it.js
@@ -11,12 +11,37 @@
    */
   window.addEventListener("load", init);
 
+
   /**
    * TODO: Write a function comment using JSDoc.
    */
   function init() {
+    document.getElementById("encrypt-it").addEventListener("click", handlerClick);
     // Note: In this function, we usually want to set up our event handlers
     // for UI elements on the page.
+  }
+
+  function handlerClick(){
+   var str = document.getElementById("input-text").value;
+   str = shiftCipher(str);
+   document.getElementById("result").innerHTML = str;
+    }
+
+  function shiftCipher(text) {
+    text = text.toLowerCase();
+    let result = "";
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] < 'a' || text[i] > 'z') {
+        result += text[i];
+      } else if (text[i] == 'z') {
+        result += 'a';
+      } else { // letter is between 'a' and 'y'
+        let letter = text.charCodeAt(i);
+        let resultLetter = String.fromCharCode(letter + 1);
+        result += resultLetter;
+      }
+    }
+    return result;
   }
 
   // Add any other functions in this area (you should not implement your
